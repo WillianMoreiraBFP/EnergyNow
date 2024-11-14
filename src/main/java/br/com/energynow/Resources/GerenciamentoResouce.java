1c3d99f59ed2337cf66449252820765a84112d1a
Willian Moreira Brito Farias Pinto <willianmbfp@gmail.com>
2024-11-14 11:17:12 -0300

    .

---
+++ src/main/java/br/com/energynow/Resources/GerenciamentoResouce.java
@@ -0,0 +1,4 @@
+package br.com.energynow.Resources;
+
+public class GerenciamentoResouce {
+}
--- src/main/java/br/com/energynow/Resources/UserResource.java
+++ src/main/java/br/com/energynow/Resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.energynow.Resources;
 
 import br.com.energynow.Service.UserService;
+import br.com.energynow.model.User;
 import br.com.energynow.model.user.User;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
@@ -10,6 +11,54 @@ import java.sql.SQLException;
 
 @Path("usuarios")
 public class UserResource {
+
+    @Path("cadastro")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response cadastrar(User user) {
+        UserService userService = new UserService ();
+
+        try{
+            user = userService.cadastro (user);
+            return Response.ok (user).build ();
+
+        } catch (SQLException e) {
+            if (e.getErrorCode () == 1) {
+                return Response.status (Response.Status.CONFLICT)
+                        .entity ("{\"error\":\"Usuário já cadastrado.\"}")
+                        .build ();
+            } else {
+                return Response.status (Response.Status.INTERNAL_SERVER_ERROR)
+                        .entity ("{\"error\":\"Erro inesperado: " + e.getMessage () + "\"}")
+                        .build ();
+            }
+        }
+    }
+
+    @POST
+    @Path("login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(User user) {
+        UserService userService = new UserService ();
+
+        try {
+            user = userService.login (user);
+
+            return Response.ok(user).build();
+        }catch (SQLException e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("{\"error\":\"Usuário não encontrado ou senha incorreta.\"}")
+                    .build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("{\"error\":\"Erro inesperado: " + e.getMessage() + "\"}")
+                    .build();
+        }
+    }
+
+
+
 
 
 }
