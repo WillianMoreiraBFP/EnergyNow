aad6364d0712f53a093a0b9939bb4f07f88ea5e9
Willian <willianmbfp@gmail.com>
2024-11-18 18:13:47 -0300

    .

--- src/main/java/br/com/energynow/Service/GerenciamentoService.java
+++ src/main/java/br/com/energynow/Service/GerenciamentoService.java
@@ -9,10 +9,7 @@ import br.com.energynow.model.Gerenciamento;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 public class GerenciamentoService {
